49fc021cbc306e258ddb9d769e98d0076a979152
rtm29331 <115970853+rtm29331@users.noreply.github.com>
2022-12-03 17:45:32 +0900

    問題つくった

--- js/main.js
+++ js/main.js
@@ -8,13 +8,12 @@
 	const chart = document.getElementById('chart');
 
 	const quizSet = shuffle([ 
-    {q: 'preiswert', c: ['正解', 'teuer', 'nett']},
-    {q: 'sich auf den Weg machen', c: ['正解', 'umziehen', 'brechen']},
-		{q: 'etwas anderes im Kopf haben', c: ['正解', 'mit jemandem Kontakt aufnehmen', 'jemanden einstellen oder für ein Studium zulassen']},
-		{q: 'テスト問題つくりました', c: ['正解', '不正解', '不正解']},		
-    {q: 'preiswert', c: ['正解', 'teuer', 'nett']},
-    {q: 'sich auf den Weg machen', c: ['正解', 'umziehen', 'brechen']},
-		{q: 'etwas anderes im Kopf haben', c: ['正解', 'mit jemandem Kontakt aufnehmen', 'jemanden einstellen oder für ein Studium zulassen']},
+    {q: 'console.log ( 1 + "2" );', c: ['12', '2', '1']},
+    {q: 'console.log ( 1 - "2" );', c: ['-1', '-12', '1']},
+		{q: 'HTMLにおいて、デフォルトでは縦並びにならないものを次のうちから1つ選んでください。', c: ['a要素', 'form要素', 'div要素']},
+		{q: 'アクションの説明として正しいものを次のうちから選んでください。', c: ['ルーティングが割り当てられ、コントローラーに定義されるメソッド', 'クラス外から呼び出すことのできないメソッド', 'コントローラーが最終的にレスポンスとして返す、見た目を決めるファイル']},		
+    {q: '「【 A 】は、値の設計図となるもの。【 A 】には共通の【 B 】を【 C 】変数として定義することで、【 C 】メソッドで個別の【 B 】値を扱うことができる」', c: ['A: クラス, B: 属性, C: インスタンス', 'A: インスタンス, B: 属性, C: initialize', 'A: クラス, B: メソッド, C: インスタンス']},
+		{q: '今日は何曜日？', c: ['金曜日', '眠い', 'お腹すいた']},
 		{q: 'テスト問題つくりました', c: ['正解', '不正解', '不正解']},		
     
 
